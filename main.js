63890b7703c363014316947dbf9dec4a0aeb440e
kani81 <kaniarasiduraisamy.com>
2023-07-20 15:23:13 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=[11,10,70,75,59,2,4];
+document.write("a=[11,10,70,75,59,2,4]"+"<br>"+"<br>");
+b=[];
+for(let i=0; i<a.length; i++){
+		if(a[i]%2==0){
+			
+			b.push(a[i]);
+			
+			
+		}
+}
+console.log(b);
